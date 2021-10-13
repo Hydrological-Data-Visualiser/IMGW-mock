e4728a4e5101b775d2487e85371a8da9afa1a45c
Przemysław Lechowicz <lechowiczprzemyslaw@gmail.com>
2021-10-13 23:04:50 +0200

    INZ-20 Endpoint to get precitipation data (#2)

--- src/main/java/pl/edu/agh/imgwmock/repository/DailyPrecipitationRepository.java
+++ src/main/java/pl/edu/agh/imgwmock/repository/DailyPrecipitationRepository.java
@@ -3,8 +3,13 @@ package pl.edu.agh.imgwmock.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.agh.imgwmock.model.DailyPrecipitation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DailyPrecipitationRepository extends JpaRepository<DailyPrecipitation, Long> {
     List<DailyPrecipitation> findByStationId(Long id);
+
+    List<DailyPrecipitation> findByStationIdAndDate(Long id, LocalDate date);
+
+    List<DailyPrecipitation> findByDate(LocalDate date);
 }
