6fb7ad2571b5e7b37dcb37ea14f7a27f45fdd260
przemq20 <lechowiczprzemyslaw@gmail.com>
2021-10-18 19:15:01 +0200

    Kocinka as endpoint - new ctroller

--- src/main/java/pl/edu/agh/imgwmock/utils/CSVUtils.java
+++ src/main/java/pl/edu/agh/imgwmock/utils/CSVUtils.java
@@ -3,6 +3,7 @@ package pl.edu.agh.imgwmock.utils;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import pl.edu.agh.imgwmock.model.DailyPrecipitation;
+import pl.edu.agh.imgwmock.model.RiverPoint;
 import pl.edu.agh.imgwmock.model.Station;
 
 import java.io.FileReader;
@@ -10,6 +11,8 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CSVUtils {
     public static List<DailyPrecipitation> getDailyPrecipitationListFromCSV(String pathToFile) {
@@ -58,5 +61,26 @@ public class CSVUtils {
             e.printStackTrace();
             return List.of();
         }
+    }
+
+    public static List<RiverPoint> getKocinka(String pathToFile) {
+        List<RiverPoint> kocinka = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(pathToFile))) {
+            List<String[]> csvRecords = reader.readAll();
+            AtomicReference<Long> lastId = new AtomicReference<>(0L);
+            Random random = new Random();
+            csvRecords.forEach(record -> {
+                kocinka.add(new RiverPoint(
+                        lastId.getAndSet(lastId.get() + 1),
+                        Double.parseDouble(record[0]),
+                        Double.parseDouble(record[1]),
+                        random.nextDouble() * 5
+                ));
+            });
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+            return List.of();
+        }
+        return kocinka;
     }
 }
